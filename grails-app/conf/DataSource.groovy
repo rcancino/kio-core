3605d148ad3e08e60f244f06e14b35760e0c9026
Ruben Cancino <rubencancino6@gmail.com>
2016-12-29 12:17:52 -0600

    Ajuste de base de datos

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -37,7 +37,7 @@ environments {
                   //dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
                 //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
               dbCreate="update"
-              url="jdbc:mysql://localhost/kyo?autoReconnect=true"
+              url="jdbc:mysql://localhost/kyo_dev?autoReconnect=true"
               driverClassName = "com.mysql.jdbc.Driver"
               dialect = org.hibernate.dialect.MySQL5InnoDBDialect
               username = "root"
